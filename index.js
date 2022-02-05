df72537a2f471fc4fc89461a3b703cfdf73a56bb
nickfff-dev <hunchographics@gmail.com>
2022-02-05 03:22:33 +0300

    last

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ const app = express()
 const port =  PORT || 5000;
 
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  
+  }) }
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
